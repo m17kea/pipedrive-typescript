bd30de5ce41b695a204d21904560f9ff799c4ebc
michael.armitage <michael.armitage@derivitec.com>
2022-10-07 14:34:23 +0100

    align for npm

--- generate_openapi_typescript.sh
+++
@@ -1,8 +0,0 @@
-export JAVA_OPTS="-Dio.swagger.parser.util.RemoteUrl.trustAll=true -Dio.swagger.v3.parser.util.RemoteUrl.trustAll=true" 
-echo $JAVA_OPTS
-openapi-generator generate \
--i https://developers.pipedrive.com/docs/api/v1/openapi.json \
--g typescript-fetch \
--c openapi_typescript_config.json \
--o ./pipedrive-client-v2 \
---skip-validate-spec
--- generate_swagger_typescript.sh
+++ generate_swagger_typescript.sh
@@ -4,7 +4,6 @@ swagger-codegen generate \
 -i https://developers.pipedrive.com/docs/api/v1/openapi.json \
 -l typescript-fetch \
 -c ./swagger_typescript_config.json \
--o ./pipedrive-client \
 -Dio.swagger.parser.util.RemoteUrl.trustAll=true \
 -Dio.swagger.v3.parser.util.RemoteUrl.trustAll=true
 
